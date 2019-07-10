a3545e8dbed2dab6757a2b9558f108be2b098f88
whuang022nccu <whuang022.nccu@gmail.com>
2019-07-10 17:03:59 +0800

    add gtest

--- api/api.cpp
+++ api/api.cpp
@@ -1,8 +1,4 @@
 
-double sum(double input1, double input2){
-    return input1+input2;
-}
+double sum(double input1, double input2) { return input1 + input2; }
 
-double sub(double input1, double input2){
-    return input1+input2;
-}
+double sub(double input1, double input2) { return input1 + input2; }
